aae12f6ca8cd91c67be379925b244de034fc1f8a
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2023-11-17 17:10:01 -0300

    Correcciones

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const fname = document.getElementById('fname');
-const lname = document.getElementById('lname');
-const email = document.getElementById('email');
-const subject = document.getElementById('subject');
-const message = document.getElementById('message');
-const formSubmit = document.getElementsByClassName('formSubmit')[0];
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const message = document.getElementById('message')
+const formSubmit = document.getElementsByClassName('formSubmit')[0]
 
 formSubmit.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("Click");
+    e.preventDefault()
+    console.log("Click")
 
     let contentBody = `
-        <b>Nombre: </b> ${fname.value}&nbsp;${lname.value}
+        <b>Nombre: </b> ${fname.value}&nbsp${lname.value}
         <br>
         <b>Correo Electrónico: </b> ${email.value}
         <br>
@@ -18,7 +18,7 @@ formSubmit.addEventListener("submit", (e) => {
         <br>
         <b>Mensaje: </b> ${message.value}
         <br>
-    `;
+    `
 
     Email.send({
         SecureToken: "a94829c9-9150-4ded-bc6a-3032b64a5200",
@@ -29,39 +29,45 @@ formSubmit.addEventListener("submit", (e) => {
     }).then(
         function () {
             setTimeout(function () {
-                window.location.href = "#Contacto";
-                fname.value = "";
-                lname.value = "";
-                email.value = "";
-                subject.value = "";
-                message.value = "";
-            }, 1000);
+                window.location.href = "#Contacto"
+                fname.value = ""
+                lname.value = ""
+                email.value = ""
+                subject.value = ""
+                message.value = ""
+            }, 1000)
         }
-    );
-});
+    )
+})
 
 document.addEventListener("DOMContentLoaded", function () {
-    const flechaAbajo = document.getElementById('arrowDown');
+    const flechaAbajo = document.getElementById('arrowDown')
 
-    // Agregar clase para la primera carga
     if (!document.body.classList.contains('primera-carga')) {
-        document.body.classList.add('primera-carga');
+        document.body.classList.add('primera-carga')
 
-        // Esperar 2 segundos antes de ejecutar la animación inicial
         setTimeout(function () {
-            // Agregar la clase de animación inicial
-            flechaAbajo.classList.add('bounce-once');
-        }, 2000); // 2000 milisegundos (2 segundos)
+            flechaAbajo.classList.add('bounce-once')
+        }, 2000)
     }
+})
 
-    // Resto del código...
-});
-
-// Resto del código...
-
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId)
+    section.scrollIntoView({ behavior: 'smooth' })
+}
 
 document.getElementById('arrowDown').addEventListener('click', function() {
-    const sobreMiSection = document.getElementById('Sobre-Mi');
-    sobreMiSection.scrollIntoView({ behavior: 'smooth' });
-    this.classList.remove('bounce-once');
-});
+    scrollToSection('Sobre-Mi')
+    this.classList.remove('bounce-once')
+})
+
+const navLinks = document.querySelectorAll('.scroll-link')
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault()
+        const targetId = this.getAttribute('href').substring(1)
+        scrollToSection(targetId)
+    })
+})
